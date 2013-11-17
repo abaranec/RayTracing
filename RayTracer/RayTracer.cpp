2f0177bb7c53c263f0e99a4725201e3de99ac525
Andrew <abaranec@optonline.net>
2013-11-16 22:20:18 -0500

    Staged preliminary tracing files

---
+++ RayTracer/RayTracer.cpp
@@ -0,0 +1,9 @@
+// RayTracer.cpp : Defines the entry point for the console application.
+//
+#define WINDOWS_LEAN_AND_MEAN
+#include <Windows.h>
+
+int _tmain(int argc, char* argv[])
+{
+	return 0;
+}
